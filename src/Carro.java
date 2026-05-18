962f684fdd39d4bdfc2cec45879fc856d0fdc3ea
André <andrecjuniorr@gmai.com>
2026-05-18 20:47:55 -0300

    commit inicial

---
+++ src/Carro.java
@@ -0,0 +1,9 @@
+// ATRIBUTOS E CARACTERISTICAS DO TIPO CARRO
+public class Carro {
+//NUNCA COLOQUE O NOME DO OBJETO NA IDENTIFICAÇÃO DO MESMO.
+    String fabricante;
+    String modelo;
+    String cor;
+    int anoFabricacao;
+
+}
